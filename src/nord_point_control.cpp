d5a701de1367732ff8d36d313163f58081e87ed9
Forrest Gump <vahiki123@gmail.com>
2015-11-11 18:36:28 +0100

    lab version 11/11/2015- Working

--- src/nord_point_control.cpp
+++ src/nord_point_control.cpp
@@ -8,6 +8,7 @@
 #include "nord_messages/MotorTwist.h"
 #include "nord_messages/NextNode.h"
 #include "nord_messages/PoseEstimate.h"
+#include <math.h>
 
 #include "pid.hpp"
 class PointControl
@@ -24,7 +25,7 @@ class PointControl
 	PointControl(char ** argv){
 		next_node_sub=n.subscribe("/nord/control/point",1,&PointControl::NextNodeCallback, this);
 		position_sub=n.subscribe("/nord/estimation/gaussian",1,&PointControl::PositionCallback, this);
-		twist_pub = n.advertise<nord_messages::MotorTwist>("/motor_controller/twist", 1);
+		twist_pub = n.advertise<nord_messages::MotorTwist>("/nord/motor_controller/twist", 1);
 		reach_pub= n.advertise<std_msgs::Bool>("/nord/houston/mission_result", 1);
 		
 		twist.velocity=0;
@@ -47,8 +48,8 @@ class PointControl
 		i_vel	= std::stod(argv[2]); 	i_ang = std::stod(argv[5]);
 		d_vel = std::stod(argv[3]);   d_ang = std::stod(argv[6]);*/
 		
-		p_vel=2.1; i_vel=0.2; d_vel=-0.16;
-		p_ang=3; i_ang=0.1; d_ang=-0.05;
+		p_vel=0.6; i_vel=0.07; d_vel=-0.04;
+		p_ang=1.5; i_ang=0.3; d_ang=-0.1;
 		
 		vel_pid =kontroll::pid<float>(p_vel, i_vel, d_vel);
 		vel_pid.max =  0.6;//0.7 its equal to a PWM of approximately 160 and considering the 45º degree start moving forward this is the limit
@@ -66,9 +67,11 @@ class PointControl
 				vec_degree[x]=-25*pi/(180*(x+1));
 			}
 		}*/
-		vec_x={1.0,1.6,1.2};
-		vec_y={0.23,0.23,0.23};
+		vec_x={2.25,2.25,1.83,1.83,1.83,2.25,2.25,0.795};
+		vec_y={0.23,0.7,0.7,1.25,0.7,0.7,0.23,0.23};
 		vec_i=0;
+		next_x=vec_x[vec_i];
+		next_y=vec_y[vec_i];
 		move=1;
 
 		// for (auto& e : vec_degree)
@@ -82,7 +85,10 @@ class PointControl
 		
 		twist.velocity= vel_pid(des_dist,dist_point , dt);
    		twist.angular_vel = ang_pid(pos_dir, dir_point, dt);
-
+		
+		if(des_dist==dist_point){
+			twist.velocity=0;
+		}
 	/*	if(real_vel<0.3 && real_vel>0){
 			real_vel=0;
 		}else if(real_vel>-0.3 && real_vel<0){
@@ -105,32 +111,55 @@ class PointControl
 	void PositionCallback(const nord_messages::PoseEstimate command){
 		pos_x=command.x.mean;
 		pos_y=command.y.mean;
-		pos_dir=command.theta.mean;
+		pos_dir=-command.theta.mean;
 		duration=command.stamp-old;
 		dt=duration.toSec();
 		old=command.stamp;
 		
-		float startmove=pi/12;
-		float dist_thres=0.05;
+		float startmove=pi/7;
+		float dist_thres=0.095;
+
+		double x1,y1;
+		double x,y;
+
+		x1=next_x-pos_x;
+		y1=next_y-pos_y;
+		x=(cos(pos_dir)*x1)-(sin(pos_dir)*y1);
+		y=(sin(pos_dir)*x1)+(cos(pos_dir)*y1);	
 		
-		
-		dist_point=sqrt(pow(float(next_x-pos_x),2.0)+pow(float(next_y-pos_y),2.0));
-		dir_point=atan2(float(next_y-pos_y),float(next_x-pos_x));
-		
-		if(dist_point<dist_thres){
+		dist_point=sqrt(pow((next_x-pos_x),2.0)+pow((next_y-pos_y),2.0));
+		dir_point=atan2(y,x);
+		ROS_INFO("dir_point: [%f] pos_dir:%f", dir_point,pos_dir);
+		/*ROS_INFO("dist_point:%f",dist_point);
+		ROS_INFO("pos_x: %f pos_y: %f",pos_x,pos_y);
+		ROS_INFO("next_x: %f next_y: %f",next_x,next_y);*/
+		ROS_INFO("x: %f y: %f",x,y);
+
+		pos_dir=0;
+
+		if(dir_point>pi){
+			dir_point-=2*pi;
+		}else if(dir_point<-pi){
+			dir_point+=2*pi;
+		}
+
+		if(dist_point<=dist_thres){
 			msg_bool.data=true;
 			reach_pub.publish(msg_bool);
 			vec_i++;
+			if(vec_i>7){
+				move=0;
+			}
 			next_x=vec_x[vec_i];
 			next_y=vec_y[vec_i];
 		}
 		
 		if(move==1){
-			if((dir_point-pos_dir)>pi){
+			/*if((dir_point-pos_dir)>pi){
 				dir_point-= (2*pi);
-			}else if((dir_point-pos_dir)>-pi){
+			}else if((dir_point-pos_dir)<-pi){
 				dir_point+= (2*pi);
-			}
+			}*/
 			if((dir_point-pos_dir)>(startmove)||(dir_point-pos_dir)<-(startmove)){
 				dist_point=0;
 			}
@@ -143,34 +172,32 @@ class PointControl
 				dir_point+=pi;
 			}
 			
-			if((dir_point-pos_dir)>pi){
+			/*if((dir_point-pos_dir)>pi){
 				dir_point-= (2*pi);
-			}else if((dir_point-pos_dir)>-pi){
+			}else if((dir_point-pos_dir)<-pi){
 				dir_point+= (2*pi);
-			}
+			}*/
 			
 			if((dir_point-pos_dir)>(startmove)||(dir_point-pos_dir)<-(startmove)){
 				dist_point=0;
 			}
 		}else{
-			dist_point=0;
+			dist_point=des_dist;
 			dir_point=pos_dir;
 		}
-		
+		//ROS_INFO("dir_point: [%f] pos_dir:%f", dir_point,pos_dir);
 		ControlPart();
 	}
 	
 	void print_info(){
 		ROS_INFO("vel: [%f]", twist.velocity);
  		ROS_INFO("ang_vel: [%f]", twist.angular_vel);
+		ROS_INFO("dir_point: [%f] pos_dir:%f", dir_point,pos_dir);
+		ROS_INFO("dist_point:%f",dist_point);
 		ROS_INFO("pos_x: %f pos_y: %f",pos_x,pos_y);
 		ROS_INFO("next_x: %f next_y: %f",next_x,next_y);
- 		ROS_INFO("des_dist: [%f]", des_dist);
- 		ROS_INFO("dist_point: [%f]", dist_point);
- 		ROS_INFO("pos_dir: [%f]", pos_dir);
-		ROS_INFO("dir_point: [%f]", dir_point);
- 		ROS_INFO("p_vel: %f i_vel:%f  d_vel:%f ",p_vel,i_vel,d_vel);
- 		ROS_INFO("p_ang: %f i_ang:%f  d_ang:%f ",p_ang,i_ang,d_ang);
+ 		//ROS_INFO("p_vel: %f i_vel:%f  d_vel:%f ",p_vel,i_vel,d_vel);
+ 		//ROS_INFO("p_ang: %f i_ang:%f  d_ang:%f ",p_ang,i_ang,d_ang);
 	}
 	
 	private:
@@ -183,9 +210,9 @@ class PointControl
 		double next_x,next_y;
 		int move;
 		double pos_x,pos_y;
-		float pos_dir;
-		float pi;
-		float dir_point,des_dist,dist_point;
+		double pos_dir;
+		double pi;
+		double dir_point,des_dist,dist_point;
 		double dt;
 		float p_vel,i_vel,d_vel; float p_ang,i_ang,d_ang;
 		kontroll::pid<float> vel_pid; kontroll::pid<float> ang_pid;
